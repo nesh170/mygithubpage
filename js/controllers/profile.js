cb7b838b6e7f91671cbf37ac5461688398712799
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-12-21 15:07:44 -0500

    added the profile

--- js/controllers/profile.js
+++ js/controllers/profile.js
@@ -1,3 +1,17 @@
 app.controller("profileController", function($scope,$log){
-    $log.log("in the profile controller");
+    var imageArray = [
+        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_1.jpg',
+        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_2.jpg',
+        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_3.jpg'
+    ];
+    $scope.profileImagePath = imageArray[0];
+
+    $scope.changePicture = function(picIndex){
+        if(picIndex > (imageArray.length -1)){
+            $log.error("The index is higher than the size of the array, index:" + picIndex + " maximum index of array: " + (imageArray.length-1));
+        }
+        else{
+            $scope.profileImagePath = imageArray[picIndex];
+        }
+    }
 });
