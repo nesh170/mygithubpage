eaed1f42c5776fb3ff76c7f3b25eda2a3ea548fb
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-12-21 17:45:18 -0500

    changed to support random picz :D

--- js/controllers/profile.js
+++ js/controllers/profile.js
@@ -1,17 +1,13 @@
 app.controller("profileController", function($scope,$log){
-    var imageArray = [
-        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_1.jpg',
-        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_2.jpg',
-        'https://s3-us-west-2.amazonaws.com/neshsolutionswebpage/profile_picture_3.jpg'
-    ];
-    $scope.profileImagePath = imageArray[0];
 
-    $scope.changePicture = function(picIndex){
-        if(picIndex > (imageArray.length -1)){
-            $log.error("The index is higher than the size of the array, index:" + picIndex + " maximum index of array: " + (imageArray.length-1));
+    $scope.currentProfilePicture = 1;
+
+    $scope.changePicture = function(){
+        var randomNumber = Math.floor((Math.random() * 4) + 1);
+        while(randomNumber == 5 || randomNumber == $scope.currentProfilePicture){
+            randomNumber = Math.floor((Math.random() * 4) + 1);
         }
-        else{
-            $scope.profileImagePath = imageArray[picIndex];
-        }
+        $scope.currentProfilePicture = randomNumber
     }
+
 });
